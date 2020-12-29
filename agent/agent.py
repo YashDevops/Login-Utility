ffea07789a96d1a8138480d1502fe75a37222743
yash shah <yashkumarshah22@gmail.com>
2020-12-29 13:11:32 +0530

    Added helper details to parse string from external command and create and event list
    - Added metadata_fetcher to add metadata for events in reponse
    - Added helper.py to create methods to parse string into a defined list

--- agent/agent.py
+++ agent/agent.py
@@ -1,9 +1,30 @@
 from flask import Flask , request
 from flask_restful import Resource, Api
+import logging
 import uuid
 import json
+import helper
 import subprocess
 
+
+# DEBUG: Detailed information, typically of interest only when diagnosing problems.
+
+# INFO: Confirmation that things are working as expected.
+
+# WARNING: An indication that something unexpected happened, or indicative of some problem in the near future (e.g. ‘disk space low’). The software is still working as expected.
+
+# ERROR: Due to a more serious problem, the software has not been able to perform some function.
+
+# CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
 
 app=Flask(__name__)
 api=Api(app)
@@ -12,12 +33,18 @@ api=Api(app)
 
 class Datasource(Resource):
     def get(self):
-        my_dict = {}
-        process = subprocess.Popen("last",shell=True ,stdout = subprocess.PIPE)
-        process.wait()
-        data, err = process.communicate()
-        data=data.decode("utf-8").split("\n")
-        return data
+        '''
+            get function to fetch logging mertics from the server and parse it if the command exit code it 0
+        '''
+        try:
+            process = subprocess.run("last",shell=True ,stdout = subprocess.PIPE, text=True)
+            if process.returncode == 0:
+                logging.debug("Command executed successfully sending stdout for parsing")
+                output=helper.parser(process.stdout)
+            return output
+        except exception as e:
+            logging.debug("The exist code is not 0 {}".format(process.stderr))
+            print(process.stderr)
 
 
 
---
+++ agent/helper.py
@@ -0,0 +1,43 @@
+import os
+from requests import get
+
+ip = get('https://api.ipify.org').text
+print('My public IP address is: {}'.format(ip))
+
+def parser(string):
+    '''
+        The following method parses the string and format it to a nested listed events
+
+        params : string : <type(str)>
+    '''
+    result = []
+    events = []
+    try:
+        metadata=metadata_fetcher()
+        data = string.split("\n")
+        for each_login in range(len(data)):
+            new_data = data[each_login].split()
+            events.append(new_data)
+        print(metadata[0]['public_ip'])
+        result.append({"public_ip":metadata[0]['public_ip'],"hostname":metadata[0]['hostname'],"release":metadata[0]['release'],"sysname":metadata[0]['sysname'],"events":events})
+        return result
+    except exception as e:
+        logger.debug("something went wrong",exception)
+        return "something went wrong"
+
+
+
+def metadata_fetcher():
+    '''
+        The following method fetches the public ip for the given machine and also generate a metadata json to be send via events
+
+    '''
+    metadata = []
+    data=os.uname()
+    try:
+        ip = get('https://api.ipify.org').text
+        metadata.append({"public_ip":ip,"hostname":data.nodename,"release":data.release,"arch":data.machine,"sysname":data.sysname})
+        return metadata
+    except exception as e:
+        logger.debug("While fetching the public ip via metadata following error occured",exception)
+        return exception
